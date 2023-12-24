56b3d891f80ba23cae884878be7b9f94a6163f5d
Rahul Lad <rahulladrs17@gmail.com>
2023-12-24 15:56:16 -0700

    add useState as value object

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,23 @@
-sdsdsdadadasdasdadasddhttps://github.com/Rahul7315/nox_blogsdsdsd
+import { useState } from "react";
+
+const Home = () => {
+    
+   const [blogs, setBlogs] = useState(
+    [{id:1, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"},
+    {id:3, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"},
+    {id:2, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"}]
+   );
+  
+    return(
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <h3>{blog.author}</h3>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Home;
