21e1a43d641e5d19c232939a71b4ac8069ca9eef
Rahul Lad <rahulladrs17@gmail.com>
2023-12-24 19:39:38 -0700

    reuse of component

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,16 @@ import Blog_list from "./blog_list";
 const Home = () => {
     
    const [blogs, setBlogs] = useState(
-    [{id:1, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"},
-    {id:3, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"},
-    {id:2, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"chatGPT"}]
+    [{id:1, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"John"},
+    {id:3, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"Maric"},
+    {id:2, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"Lucy"}]
    );
   
     return(
+        <div className="home">
         <Blog_list blogs={blogs} title={"All Blogs"}/>
+        <Blog_list blogs={blogs.filter((blog)=> blog.author === 'Lucy')} title={"Lucy's Blog"}/>
+        </div>
     );
 }
 
