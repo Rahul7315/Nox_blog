9e074dd4d01b6ac435ab8e39034e6ae42cfd70be
Rahul Lad <rahulladrs17@gmail.com>
2023-12-26 19:43:29 -0700

    useEffect dependcy

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,28 @@ import { useState, useEffect } from "react";
 import Blog_list from "./blog_list";
 const Home = () => {
     
-   const [blogs, setBlogs] = useState(
+   const [name, setName] = useState("Mario")
+    const [blogs, setBlogs] = useState(
     [{id:1, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"John"},
     {id:3, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"Maric"},
     {id:2, title:"Exploring the Wonders of Serendipity", details:"Imagine wandering through the bustling", author:"Lucy"}]
    );
 
-    //use effect will run on every time when ever page load or we can set up with condition like data added successfully as well
-    // data did'not load in that case we can use it.
-   useEffect(() => {
-    console.log("Run the useEffect");
-   })
-  
+    useEffect(()=> {
+        console.log("Name changed")
+        
+    },[name]);
+    //use dependicies whenever page load , use effect is load defaul, but after that when we click on change the but
+    // at that time useEffect will run again.
+
+
     return(
         <div className="home">
         <Blog_list blogs={blogs} title={"All Blogs"}/>
         <Blog_list blogs={blogs.filter((blog)=> blog.author === 'Lucy')} title={"Lucy's Blog"}/>
+        <p>{name}</p>
+        <button onClick={()=>setName("Rahul")}>Change the name</button>
+       
         </div>
     );
 }
